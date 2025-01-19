8f98af383244075002c01ed50e73ae50efbee522
DevGabriel0402 <gabriel.profissional01@gmail.com>
2025-01-18 21:18:54 -0300

    Subindo arquivos para git

---
+++ app.js
@@ -0,0 +1,48 @@
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("box-result").style.opacity = "0";
+    document.getElementById("close-result").style.animation = " scaleDown .3s forwards";
+
+
+})
+
+
+const limparDados = () => {
+    document.getElementById("box-result").style.animation = "fadeInUp .3s forwards";
+    document.getElementById("inputCEP").value = "";
+    document.getElementById("close-result").style.animation = " scaleDown .3s forwards";
+
+
+};
+document.getElementById("pesquisarCEP").onclick = async () => {
+    const cep = document.getElementById("inputCEP").value.trim();
+
+    if (!/^\d{8}$/.test(cep)) {
+        alert("Por favor, insira um CEP válido com 8 dígitos.");
+        limparDados()
+        return;
+    }
+    try {
+        const response = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
+        if (!response.ok) throw new Error("Erro ao consultar o CEP.");
+        const data = await response.json();
+
+        if (data.erro) {
+            alert("CEP não encontrado.");
+            limparDados();
+            return;
+        }
+        document.getElementById("cep").value = `CEP - ${data.cep}` || "";
+        document.getElementById("rua").value = `Logradouro: ${data.logradouro}` || "";
+        document.getElementById("bairro").value = `Bairro: ${data.bairro}` || "";
+        document.getElementById("cidade").value = `Cidade: ${data.localidade}` || "";
+        document.getElementById("estado").value = `Estado: ${data.uf}` || "";
+        document.getElementById("box-result").style.animation = "fadeInDown .3s forwards";
+        document.getElementById("close-result").style.animation = " scaleIn .3s forwards";
+
+
+    } catch (error) {
+        console.error("Erro na consulta:", error.message);
+        alert("Erro ao consultar o CEP. Tente novamente mais tarde.");
+        limparDados();
+    }
+};
